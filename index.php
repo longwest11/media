fe2d917c28a0f045f594f2fc46621e02c351fecb
長西大輝 <>
2020-07-22 11:58:02 +0900

    ファイルをアップ

---
+++ index.php
@@ -0,0 +1,291 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>CINEMA.LOG | TOP</title>
+    <!-- BootstrapのCSS読み込み -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- jQuery読み込み -->
+    <script src="js/jquery-3.4.1.min.js"></script>
+    <!-- BootstrapのJS読み込み -->
+    <script src="js/bootstrap.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="slick-1.8.1/slick/slick.css">
+<link rel="stylesheet" type="text/css" href="slick-1.8.1/slick/slick-theme.css">
+   <link rel="stylesheet" href="css/bootstrap-reboot.css">
+    <link rel="stylesheet" href="style.css">
+    
+  </head>
+  <body>
+  
+  <header>
+      <div class="header-inner">
+          <h1 class="logo"><a href="index.html"><img src="img/logo2.png" width="50%" alt=""></a></h1>
+          <ul>
+             <li class="menu-btn">
+                 <p class=" bar first"></p>
+                 <p class="bar second"></p>
+                 <p class="last"></p>
+             </li>
+              <li><a href="view/view.html">ニュース</a></li>
+              <li>映画情報</li>
+              <li>映画批評</li>
+              <li><a href="contact/contact.html">お問い合わせ</a></li>
+          </ul>
+      </div>
+      
+      
+      
+      <div class="menu-inner">
+          <ul>
+              <li><a href="index.html">HOME</a></li>
+              <li><a href="view/view.html">ニュース</a></li>
+              <li>映画情報</li>
+              <li>映画批評</li>
+              <li><a href="contact/contact.html">お問い合わせ</a></li>
+          </ul>
+      </div>
+      
+      <div class="header-mainvisual">
+          <div class="mainvisual-inner slider">
+              <section class="mainvisual-contents">
+                  <img class="sam" src="img/damy.jpg" alt="">
+                  <p class="catgory-title">ニュース</p>
+                  <h3>タイトル</h3>
+                  <p class="date">2020.04.01</p>
+                  <p class="sample-text">テキストテキストテキストテキストテキスト</p>
+              </section>
+              <section class="mainvisual-contents">
+                  <img class="sam" src="img/damy.jpg" alt="">
+                  <p class="catgory-title">ニュース</p>
+                  <h3>タイトル</h3>
+                  <p class="date">2020.04.01</p>
+                  <p class="sample-text">テキストテキストテキストテキストテキスト</p>
+              </section>
+              <section class="mainvisual-contents">
+                  <img class="sam" src="img/damy.jpg" alt="">
+                  <p class="catgory-title">ニュース</p>
+                  <h3>タイトル</h3>
+                  <p class="date">2020.04.01</p>
+                  <p class="sample-text">テキストテキストテキストテキストテキスト</p>
+              </section>
+              <section class="mainvisual-contents">
+                  <img class="sam" src="img/damy.jpg" alt="">
+                  <p class="catgory-title">ニュース</p>
+                  <h3>タイトル</h3>
+                  <p class="date">2020.04.01</p>
+                  <p class="sample-text">テキストテキストテキストテキストテキスト</p>
+              </section>
+              <section class="mainvisual-contents">
+                  <img class="sam" src="img/damy.jpg" alt="">
+                  <p class="catgory-title">ニュース</p>
+                  <h3>タイトル</h3>
+                  <p class="date">2020.04.01</p>
+                  <p class="sample-text">テキストテキストテキストテキストテキスト</p>
+              </section>
+              <section class="mainvisual-contents">
+                  <img class="sam" src="img/damy.jpg" alt="">
+                  <p class="catgory-title">ニュース</p>
+                  <h3>タイトル</h3>
+                  <p class="date">2020.04.01</p>
+                  <p class="sample-text">テキストテキストテキストテキストテキスト</p>
+              </section>
+          </div>
+      </div>
+  </header>
+  
+  <main>
+      <div class="container main-contents">
+          <div class="row">
+              <div class="col-12 col-md-7">
+                 <div class="main-contents news">
+                  <h2 class="catgory-title">ニュース<br><span>News</span></h2>
+                  <div class="row">
+                      <section class="col-12 col-md-5 kiji-contents">
+                          <a href="review/review.html"><img src="img/damy.jpg" width="100%" alt=""></a>
+                          <h3><a href="review/review.html">記事タイトル</a></h3>
+                          <p class="date">2020.04.01 | <span>News</span></p>
+                      </section>
+                      
+                      <div class="col-md-2"></div>
+                      
+                      <section class="col-12 col-md-5 kiji-contents">
+                          <img src="img/damy.jpg" width="100%" alt="">
+                          <h3>記事タイトル</h3>
+                          <p class="date">2020.04.01 | <span>News</span></p>
+                      </section>
+                      
+                      <section class="col-12 col-md-5 kiji-contents">
+                          <img src="img/damy.jpg" width="100%" alt="">
+                          <h3>記事タイトル</h3>
+                          <p class="date">2020.04.01 | <span>News</span></p>
+                      </section>
+                      
+                      <div class="col-md-2"></div>
+                      
+                      <section class="col-12 col-md-5 kiji-contents">
+                          <img src="img/damy.jpg" width="100%" alt="">
+                          <h3>記事タイトル</h3>
+                          <p class="date">2020.04.01 | <span>News</span></p>
+                      </section>
+                  </div>
+                  <a href="view/view.html"><p class="more-text">もっと見る</p></a>
+                  </div>
+                  <div class="main-contents movie-infomation">
+                  <h2 class="catgory-title">映画情報<br><span>Movie Infomation</span></h2>
+                  <div class="row">
+                      <section class="col-12 col-md-5 kiji-contents">
+                          <img src="img/damy.jpg" width="100%" alt="">
+                          <h3>記事タイトル</h3>
+                          <p class="date">2020.04.01 | <span>Movie Infomation</span></p>
+                      </section>
+                      
+                      <div class="col-md-2"></div>
+                      
+                      <section class="col-12 col-md-5 kiji-contents">
+                          <img src="img/damy.jpg" width="100%" alt="">
+                          <h3>記事タイトル</h3>
+                          <p class="date">2020.04.01 | <span>Movie Infomation</span></p>
+                      </section>
+                      
+                      <section class="col-12 col-md-5 kiji-contents">
+                          <img src="img/damy.jpg" width="100%" alt="">
+                          <h3>記事タイトル</h3>
+                          <p class="date">2020.04.01 | <span>Movie Infomation</span></p>
+                      </section>
+                      
+                      <div class="col-md-2"></div>
+                      
+                      <section class="col-12 col-md-5 kiji-contents">
+                          <img src="img/damy.jpg" width="100%" alt="">
+                          <h3>記事タイトル</h3>
+                          <p class="date">2020.04.01 | <span>News</span></p>
+                      </section>
+                  </div>
+                  <p class="more-text">もっと見る</p>
+                  </div>
+                  
+                  <div class="main-contents movie-review">
+                  <h2 class="catgory-title">映画批評<br><span>Movie Review</span></h2>
+                  <div class="row">
+                      <section class="col-12 col-md-5 kiji-contents">
+                          <img src="img/damy.jpg" width="100%" alt="">
+                          <h3>記事タイトル</h3>
+                          <p class="date">2020.04.01 | <span>Movie Review</span></p>
+                      </section>
+                      
+                      <div class="col-md-2"></div>
+                      
+                      <section class="col-12 col-md-5 kiji-contents">
+                          <img src="img/damy.jpg" width="100%" alt="">
+                          <h3>記事タイトル</h3>
+                          <p class="date">2020.04.01 | <span>Movie Review</span></p>
+                      </section>
+                      
+                      <section class="col-12 col-md-5 kiji-contents">
+                          <img src="img/damy.jpg" width="100%" alt="">
+                          <h3>記事タイトル</h3>
+                          <p class="date">2020.04.01 | <span>Movie Review</span></p>
+                      </section>
+                      
+                      <div class="col-md-2"></div>
+                      
+                      <section class="col-12 col-md-5 kiji-contents">
+                          <img src="img/damy.jpg" width="100%" alt="">
+                          <h3>記事タイトル</h3>
+                          <p class="date">2020.04.01 | <span>Movie Review</span></p>
+                      </section>
+                  </div>
+                  <p class="more-text">もっと見る</p>
+                  </div>
+              </div>
+              
+              <div class="col-md-2"></div>
+              
+              <div class="col-12 col-md-3">
+                  <div class="ranking">
+                      <h2 class="catgory-title">人気記事<br><span>Ranking</span></h2>
+                      <div class="ranking-contents">
+                          <img src="img/damy.jpg" width="50%" alt="">
+                          <p class="juni">1</p>
+                          <p class="ranking-title">タイトル</p>
+                      </div>
+                      <div class="ranking-contents">
+                          <img src="img/damy.jpg" width="50%" alt="">
+                          <p class="juni">2</p>
+                          <p class="ranking-title">タイトル</p>
+                      </div>
+                      <div class="ranking-contents">
+                          <img src="img/damy.jpg" width="50%" alt="">
+                          <p class="juni">3</p>
+                          <p class="ranking-title">タイトル</p>
+                      </div>
+                      <div class="ranking-contents">
+                          <img src="img/damy.jpg" width="50%" alt="">
+                          <p class="juni">4</p>
+                          <p class="ranking-title">タイトル</p>
+                      </div>
+                      <div class="ranking-contents">
+                          <img src="img/damy.jpg" width="50%" alt="">
+                          <p class="juni">5</p>
+                          <p class="ranking-title">タイトル</p>
+                      </div>
+                      <div class="ranking-contents">
+                          <img src="img/damy.jpg" width="50%" alt="">
+                          <p class="juni">6</p>
+                          <p class="ranking-title">タイトル</p>
+                      </div>
+                  </div>
+                  
+                  <div class="tag">
+                      <h2 class="catgory-title">タグ検索</h2>
+                      <ul class="tag-serach">
+                          <li>タグ</li>
+                          <li>食べ物</li>
+                          <li>人気</li>
+                          <li>コミュニティ</li>
+                          <li>特集</li>
+                          <li>インタビュー</li>
+                          <li>PR・広報</li>
+                      </ul>
+                  </div>
+                  
+                  <div class="contact">
+                      <h2 class="catgory-title">お問い合わせ</h2>
+                      <form action="">
+                          <div class="col-12 mb-2">
+                              <label>名前：</label><br>
+                              <input type="text" placeholder="名前">
+                              <br><label>メールアドレス</label><br>
+                              <input type="text" placeholder="sdzg514@gmail.com">
+                              <br><label>メールアドレス：確認用</label><br>
+                              <input type="text" placeholder="sdzg514@gmail.com">
+                              <br><label>コメント</label><br>
+                              <textarea name="" id="" cols="30" rows="10"></textarea>
+                          </div>
+                      </form>
+                      <p class="form-btn">確認画面へ進む</p>
+                  </div>
+              </div>
+          </div>
+      </div>
+  </main>
+  
+  <footer>
+      <div class="footer-inner">
+          <h1 class="logo"><img src="img/logo2.png" width="50%" alt=""></h1>
+          
+          <ul>
+              <li><a href="index.html">HOME</a></li>
+              <li><a href="view/view.html">ニュース</a></li>
+              <li>映画情報</li>
+              <li>映画批評</li>
+              <li><a href="contact/contact.html">お問い合わせ</a></li>
+          </ul>
+      </div>
+      <p class="copy">copyright</p>
+  </footer>
+  <script src="slick-1.8.1/slick/slick.min.js"></script>
+  </body>
+</html>
---
+++ js/script.js
@@ -0,0 +1,14 @@
+<script>
+    jQuery(function ($) {
+        
+        $('.menu-btn').on('click', function() {
+            $('.menu-inner').toggleClass('menu-inner-open');
+        
+    });
+        
+        
+        
+        
+        });
+    
+    </script>
